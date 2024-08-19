b0219b22ff7db5f9f81cd999c5ebb301617f94f1
Wiktor Kwapisiewicz <wiktor@metacode.biz>
2024-08-19 11:28:49 +0200

    Add preconditions

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,15 @@ fn main() -> testresult::TestResult {
     let result = hasher.serialize();
     eprintln!("{:?}", result);
 
-    let mut hasher = sha2::Sha256::deserialize(&result)?;
+    let hasher = sha2::Sha256::deserialize(&result)?;
     let result1 = hasher.finalize();
     eprintln!("{:?}", result1);
 
-    let mut hasher = sha2::Sha256::deserialize(&result)?;
+    let hasher = sha2::Sha256::deserialize(&result)?;
     let result2 = hasher.finalize();
     eprintln!("{:?}", result2);
+
+    assert_eq!(result1, result2);
 
     Ok(())
 }
